91c64ecbb0c029aebd4fddc3cad0891d569a1c29
Kelly Bernacki <kellyq312@yahoo.com>
2023-02-24 12:27:13 -0600

    add all elements. resize logos

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Nav Bar
+
+
+//Projects
+
+let projectImage = document.getElementsByClassName('project-item');
+const hoverProjectImg = () => {
+    projectImage.style.display = 'block';
+}
+
+projectImage.addEventListener('mouseover', hoverProjectImg);
+
+
