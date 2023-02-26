d9df869045db98731c13c0a8dddee6ffbc72acaf
Kelly Bernacki <kellyq312@yahoo.com>
2023-02-26 17:13:37 -0600

    completed site

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
+/*Commented out
+
 //Nav Bar
 const nav = document.querySelector('nav');
-const menuIcon = document.querySelector('.menu-icon');
+const menuIcon = document.querySelector('.menuicon');
 const menu = document.querySelector('.menu');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > nav.offsetHeight + 10) {
+  if (window.scrollY > 50) {
     nav.classList.add('scrolled');
-    menuIcon.style.display = 'inline-block';
-    menu.style.display = 'hidden';
+    menuIcon.style.display = 'block';
+    menu.style.display = 'none';
   } else {
     nav.classList.remove('scrolled');
-    menuIcon.style.display = 'hidden';
-    menu.style.display = 'inline-block';
+    menuIcon.style.display = 'none';
+    menu.style.display = 'block';
   }
 });
 
 
+//Contact
+
+window.addEventListener("load", function() {
+    const form = document.querySelector('formsubmit');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
 
 
 
 
-
-//Projects
-
-let projectImage = document.getElementsByClassName('project-item');
-const hoverProjectImg = () => {
-    projectImage.style.display = 'block';
-}
-
-projectImage.addEventListener('mouseover', hoverProjectImg);
-
-
-
-
+  */
