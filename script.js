af867050ebf1465b92cb4a1649b7701875f6fce6
Kelly Bernacki <kellyq312@yahoo.com>
2023-02-25 22:45:43 -0600

    add most all styling. need to work on Project section

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //Nav Bar
+const nav = document.querySelector('nav');
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > nav.offsetHeight + 10) {
+    nav.classList.add('scrolled');
+    menuIcon.style.display = 'inline-block';
+    menu.style.display = 'hidden';
+  } else {
+    nav.classList.remove('scrolled');
+    menuIcon.style.display = 'hidden';
+    menu.style.display = 'inline-block';
+  }
+});
+
+
+
+
+
 
 
 //Projects
@@ -9,5 +29,7 @@ const hoverProjectImg = () => {
 }
 
 projectImage.addEventListener('mouseover', hoverProjectImg);
+
+
 
 
